191939809628c2195f706619614e489e8e571e50
DragonMaster436 <96381483+DragonMaster436@users.noreply.github.com>
2024-02-29 22:47:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Therefore, confess your sins to one another and pray for one another, that you may be healed. The prayer of a righteous person has great power as it is working. James 5:16
+
+
+# Never saw this before, but that verse is conditional.
+
+# Play around with this if you'd like.
+
+
+# Change each True to False below based apon if you are doing those things.
+confessing_sins = True
+
+praying_for_one_another = False
+
+if confessing_sins and praying_for_one_another:
+  print("You may be healed.")
+
+else:
+  print("You may not be healed.")
